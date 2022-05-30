9dc4b2ce144c734b9b45df4d0c5ee72c2777ad24
Connor Burns <cburns1993@gmail.com>
2022-05-30 11:05:20 -0700

    heroku debugging

--- backend/app.js
+++ backend/app.js
@@ -5,16 +5,37 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { environment } = require('./config');
+const routes = require('./routes')
 const { ValidationError } = require('sequelize');
 const isProduction = environment === 'production';
-const routes = require('./routes');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(routes)
+
+
+if (!isProduction) {
+
+  app.use(cors());
+}
+
+
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
+
+app.use(routes);
+
+
 app.use((_req, _res, next) => {
   const err = new Error("The requested resource couldn't be found.");
   err.title = "Resource Not Found";
@@ -22,13 +43,17 @@ app.use((_req, _res, next) => {
   err.status = 404;
   next(err);
 });
+
+
 app.use((err, _req, _res, next) => {
+
   if (err instanceof ValidationError) {
     err.errors = err.errors.map((e) => e.message);
     err.title = 'Validation error';
   }
   next(err);
 });
+
 app.use((err, _req, res, _next) => {
   res.status(err.status || 500);
   console.error(err);
@@ -40,24 +65,6 @@ app.use((err, _req, res, _next) => {
   });
 });
 
-if (!isProduction) {
-    app.use(cors());
-  }
 
-  app.use(
-    helmet.crossOriginResourcePolicy({
-      policy: "cross-origin"
-    })
-  );
 
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
-    })
-  );
-
-  module.exports = app;
+module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const csrf = require('csurf')
-const csrfProtection = csrf({ cookie: true });
+// const csrfProtection = csrf({ cookie: true });
 const router = express.Router();
 const apiRouter = require('./api');
 
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 if (process.env.NODE_ENV !== 'production') {
-  router.get('/api/csrf/restore', csrfProtection, (req, res) => {
+  router.get('/api/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.status(201).json({});
   });
