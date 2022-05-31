e79a74d837b5480d66347ef038c44d605c0b0a67
Connor Burns <cburns1993@gmail.com>
2022-05-31 13:31:13 -0700

    more debugging for crud

--- backend/routes/api/businesses.js
+++ backend/routes/api/businesses.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-const { Business } = require('../../db/models/business');
+const { Business } = require('../../db/models/business.js');
 
 const router = express.Router();
 
 router.get('/', asyncHandler(async function(req, res) {
     const businesses = await Business.findAll({})
+    console.log('hello from routes/api/businesses')
+    console.log(businesses)
 
     return res.json(businesses)
 }))
